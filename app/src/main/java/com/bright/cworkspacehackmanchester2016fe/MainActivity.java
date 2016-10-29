b27da4a14d5a66b7e7f9e18f4e3981159df55a11
orangudan <harrison92d+orangudan@gmail.com>
2016-10-30 00:51:36 +0100

    Reverted to Java app (React Native lacks a decent Twitter auth plugin)

---
+++ app/src/main/java/com/bright/cworkspacehackmanchester2016fe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bright.cworkspacehackmanchester2016fe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
